c4223d95d84f8bad06effbe692b91709121a2bed
uramibushi <uramibushi701@gmail.com>
2022-04-10 13:14:22 +0900

    タスク状態を追加

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク状態
+    /// </summary>
+    public enum TaskStatus
+    {
+        /// <summary>
+        /// TODO（未着手）
+        /// </summary>
+        ToDo,
+
+        /// <summary>
+        /// Doing（着手）
+        /// </summary>
+        Doing,
+
+        /// <summary>
+        /// Done（完了）
+        /// </summary>
+        Done,
+    }
+}
