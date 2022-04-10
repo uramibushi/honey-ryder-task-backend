edae3ba0bff801cc3cd0291d3f9091ba1904f4c3
uramibushi <uramibushi701@gmail.com>
2022-04-10 09:08:37 +0900

    タスクID（値オブジェクト）作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskId.cs
@@ -0,0 +1,50 @@
+﻿using HoneyRyderTask.Domain.Models.Shared;
+
+namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスクID
+    /// タスクを一位に識別します。
+    /// </summary>
+    public record TaskId
+    {
+        private const int Length = 26;
+
+        private TaskId(string value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// タスクID
+        /// </summary>
+        public string Value { get; }
+
+        /// <summary>
+        /// 指定された値でタスクIDを生成します。
+        /// </summary>
+        /// <param name="value">タスクIDを</param>
+        /// <returns>タスクID</returns>
+        public static TaskId ValueOf(string value)
+        {
+            Validate(value);
+            return new TaskId(value);
+        }
+
+        /// <summary>
+        /// ULID形式の新しいタスクIDを生成します。
+        /// </summary>
+        /// <returns>
+        /// 新しく生成したタスクIDを返します。
+        /// </returns>
+        public static TaskId NewId()
+        {
+            return new TaskId(ULID.NewULID().Value);
+        }
+
+        private static void Validate(string value)
+        {
+            if (value.Length != Length) throw new Exception($"タスクIDは{Length}文字である必要があります。");
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/TaskId_Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスクID - test
+    /// </summary>
+    public class TaskId_Test
+    {
+        [Fact(DisplayName = "[ValueOf()] 指定した値でタスクIDが生成される")]
+        public void ValueOf_Test1()
+        {
+            // arrange
+            var value = "12345678901234567890123456";
+
+            // act
+            var id = TaskId.ValueOf(value);
+
+            // assert
+            Assert.Equal(value, id.Value);
+        }
+
+        [Theory(DisplayName = "[ValueOf()] 26文字以外の値を渡すと例外が発生する")]
+        [InlineData("1234567890123456789012345")]
+        [InlineData("123456789012345678901234567")]
+        public void ValueOf_Test2(string value)
+        {
+            // act
+            var target = () => TaskId.ValueOf(value);
+
+            // assert
+            var ex = Assert.Throws<Exception>(target);
+            Assert.Equal("タスクIDは26文字である必要があります。", ex.Message);
+        }
+
+        [Fact(DisplayName = "[NewId()] 新しいIDがULID形式で生成される")]
+        public void NewId_Test1()
+        {
+            // act
+            var id = TaskId.NewId();
+
+            // assert
+            Assert.Equal(26, id.Value.Length);
+        }
+    }
+}
