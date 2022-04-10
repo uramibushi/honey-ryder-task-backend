6856ac3b5f0e49bce56bf2d6b84bba60897212f5
uramibushi <uramibushi701@gmail.com>
2022-04-10 13:48:20 +0900

    タスク詳細（値オブジェクト）作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskDetail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク詳細
+    /// </summary>
+    public record TaskDetail
+    {
+        private const int MaxLength = 300;
+
+        private TaskDetail(string value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// タスク詳細
+        /// </summary>
+        public string Value { get; }
+
+        /// <summary>
+        /// 指定した値でタスク詳細を生成します。
+        /// </summary>
+        /// <param name="value">指定値</param>
+        /// <returns>
+        /// タスク詳細
+        /// </returns>
+        public static TaskDetail ValueOf(string value)
+        {
+            Validate(value);
+            return new TaskDetail(value);
+        }
+
+        private static void Validate(string value)
+        {
+            if (value.Length > MaxLength) throw new Exception($"タスク詳細は{MaxLength}文字以下である必要があります");
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/TaskDetail_Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク詳細 - test
+    /// </summary>
+    public class TaskDetail_Test
+    {
+        [Fact(DisplayName = "[ValueOf()] 指定した値でタスク詳細を生成できる")]
+        public void ValueOf_Test1()
+        {
+            // arrange
+            var value = new string('あ', 300);
+
+            // act
+            var taskDetail = TaskDetail.ValueOf(value);
+
+            // assert
+            Assert.Equal(value, taskDetail.Value);
+        }
+
+        [Fact(DisplayName = "[ValueOf()] タスク詳細は300文字以下である必要がある")]
+        public void ValueOf_Test2()
+        {
+            // arrange
+            var value = new string('あ', 301);
+
+            // act
+            var target = () => TaskDetail.ValueOf(value);
+
+            // assert
+            var ex = Assert.Throws<Exception>(target);
+            Assert.Equal($"タスク詳細は30文字以下である必要があります", ex.Message);
+        }
+    }
+}
