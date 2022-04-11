0b83137b7600433e30ee01f21ff70259be9b0202
uramibushi <uramibushi701@gmail.com>
2022-04-11 21:32:06 +0900

    作業時間（値オブジェクト）作成
    計画時間を作業時間として再作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/WorkingTime.cs
@@ -0,0 +1,44 @@
+﻿using HoneyRyderTask.Domain.Helpers.Extensions;
+
+namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// 作業時間
+    /// </summary>
+    public record WorkingTime
+    {
+        private WorkingTime(decimal value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// 作業時間（h)
+        /// </summary>
+        public decimal Value { get; }
+
+        /// <summary>
+        /// 指定した値で作業時間を生成します。
+        /// </summary>
+        /// <param name="value">指定値</param>
+        /// <returns>
+        /// 作業時間
+        /// </returns>
+        public static WorkingTime ValueOf(decimal value)
+        {
+            var adjustedValue = AdjustValue(value);
+            return new WorkingTime(adjustedValue);
+        }
+
+        /// <summary>
+        /// 値を調整する。
+        /// 作業時間の小数部は２桁までとする。３桁以上は切り捨てを行う。
+        /// </summary>
+        /// <param name="value">調整を行う値。</param>
+        /// <returns>調整後の値</returns>
+        private static decimal AdjustValue(decimal value)
+        {
+            return value.Truncate(2);
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/WorkingTime_Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// 作業時間 - test
+    /// </summary>
+    public class WorkingTime_Test
+    {
+        [Fact(DisplayName = "[ValueOf()] 指定した値で作業時間を生成できる")]
+        public void ValueOf_Test1()
+        {
+            // arrange
+            var value = 1.25M;
+
+            // act
+            var workingTime = WorkingTime.ValueOf(value);
+
+            // assert
+            Assert.Equal(value, workingTime.Value);
+        }
+
+        [Fact(DisplayName = "[ValueOf()] 指定した値の小数部３桁以上の値は切り捨てられる")]
+        public void ValueOf_Test2()
+        {
+            // arrange
+            var value = 1.255M;
+
+            // act
+            var workingTime = WorkingTime.ValueOf(value);
+
+            // assert
+            Assert.Equal(1.25M, workingTime.Value);
+        }
+    }
+}
