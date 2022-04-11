7c7ec75c8bacb36c9163499bf7023924cc331d89
uramibushi <uramibushi701@gmail.com>
2022-04-11 19:52:12 +0900

    decimal拡張メソッド作成
    ・Truncate
    ・ShiftLeft
    ・ShiftRight

---
+++ src/HoneyRyderTask.Domain/Helpers/Extensions/DecimalExtension.cs
@@ -0,0 +1,47 @@
+﻿namespace HoneyRyderTask.Domain.Helpers.Extensions
+{
+    /// <summary>
+    /// decimal（拡張メソッド）
+    /// </summary>
+    public static class DecimalExtension
+    {
+        /// <summary>
+        /// 小数値の切り捨てを行います
+        /// </summary>
+        /// <param name="d">切り捨て対象の10進数</param>
+        /// <param name="decimals">切り捨て結果の数値の小数点以下の桁数を指定します</param>
+        /// <returns>切り捨て結果の値</returns>
+        public static decimal Truncate(this decimal d, int decimals)
+        {
+            // Truncateメソッドでは切り捨てを行う小数桁数を指定できない為、
+            // 桁を左シフト -> 切り捨て -> シフトした桁を右シフトして戻す。
+            // 例) 123.456から小数桁2桁の位置で切り捨てを行う場合。
+            // 12345.6 -> 12345 -> 123.45
+            var shiftedValue = d.ShiftLeft(decimals);
+            var truncatedValue = decimal.Truncate(shiftedValue);
+            return truncatedValue.ShiftRight(decimals);
+        }
+
+        /// <summary>
+        /// 桁を左にシフトします。
+        /// </summary>
+        /// <param name="d">シフト対象の10進数</param>
+        /// <param name="shiftCount">桁移動数</param>
+        /// <returns>シフト結果の値</returns>
+        public static decimal ShiftLeft(this decimal d, int shiftCount)
+        {
+            return d * (decimal)Math.Pow(10, shiftCount);
+        }
+
+        /// <summary>
+        /// 桁を右にシフトします。
+        /// </summary>
+        /// <param name="d">シフト対象の10進数</param>
+        /// <param name="shiftCount">桁移動数</param>
+        /// <returns>シフト結果の値</returns>
+        public static decimal ShiftRight(this decimal d, int shiftCount)
+        {
+            return d / (decimal)Math.Pow(10, shiftCount);
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Helpers/Extensions/DecimalExtension_Test.cs
@@ -0,0 +1,71 @@
+﻿using HoneyRyderTask.Domain.Helpers.Extensions;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Helpers.Extensions
+{
+    /// <summary>
+    /// decimal（拡張メソッド）- test
+    /// </summary>
+    public class DecimalExtension_Test
+    {
+        [Theory(DisplayName = "[Truncate(d, decimals)] 指定した小数桁で切り捨てを行える")]
+        [InlineData(123.456, 0, 123)]
+        [InlineData(123.456, 1, 123.4)]
+        [InlineData(123.456, 2, 123.45)]
+        [InlineData(123.456, 3, 123.456)]
+        [InlineData(123.456, 4, 123.456)]
+        [InlineData(-123.456, 0, -123)]
+        [InlineData(-123.456, 1, -123.4)]
+        [InlineData(-123.456, 2, -123.45)]
+        [InlineData(-123.456, 3, -123.456)]
+        [InlineData(-123.456, 4, -123.456)]
+        public void Truncate_Test1(decimal d, int decimals, decimal expected)
+        {
+            // act
+            var actual = d.Truncate(decimals);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory(DisplayName = "[ShiftLeft(d, shiftCount)] 指定した数だけ桁を左に移動する")]
+        [InlineData(123.456, 0, 123.456)]
+        [InlineData(123.456, 1, 1234.56)]
+        [InlineData(123.456, 2, 12345.6)]
+        [InlineData(123.456, 3, 123456)]
+        [InlineData(123.456, 4, 1234560)]
+        [InlineData(-123.456, 0, -123.456)]
+        [InlineData(-123.456, 1, -1234.56)]
+        [InlineData(-123.456, 2, -12345.6)]
+        [InlineData(-123.456, 3, -123456)]
+        [InlineData(-123.456, 4, -1234560)]
+        public void ShiftLeft_Test1(decimal d, int shiftCount, decimal expected)
+        {
+            // act
+            var actual = d.ShiftLeft(shiftCount);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory(DisplayName = "[ShiftRight(d, shiftCount)] 指定した数だけ桁を右に移動する")]
+        [InlineData(123.456, 0, 123.456)]
+        [InlineData(123.456, 1, 12.3456)]
+        [InlineData(123.456, 2, 1.23456)]
+        [InlineData(123.456, 3, 0.123456)]
+        [InlineData(123.456, 4, 0.0123456)]
+        [InlineData(-123.456, 0, -123.456)]
+        [InlineData(-123.456, 1, -12.3456)]
+        [InlineData(-123.456, 2, -1.23456)]
+        [InlineData(-123.456, 3, -0.123456)]
+        [InlineData(-123.456, 4, -0.0123456)]
+        public void ShiftRight_Test1(decimal d, int shiftCount, decimal expected)
+        {
+            // act
+            var actual = d.ShiftRight(shiftCount);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
