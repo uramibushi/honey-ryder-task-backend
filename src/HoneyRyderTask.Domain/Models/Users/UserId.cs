1216d446572042de9a0564a21898031c686883c2
uramibushi <uramibushi701@gmail.com>
2022-04-12 22:36:14 +0900

    ユーザーID（値オブジェクト）作成

---
+++ src/HoneyRyderTask.Domain/Models/Users/UserId.cs
@@ -0,0 +1,50 @@
+﻿using HoneyRyderTask.Domain.Models.Shared;
+
+namespace HoneyRyderTask.Domain.Models.Users
+{
+    /// <summary>
+    /// ユーザーID
+    /// ユーザーを一位に識別します。
+    /// </summary>
+    public record UserId
+    {
+        private const int Length = 26;
+
+        private UserId(string value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// ユーザーID
+        /// </summary>
+        public string Value { get; }
+
+        /// <summary>
+        /// 指定された値でユーザーIDを生成します。
+        /// </summary>
+        /// <param name="value">指定値</param>
+        /// <returns>ユーザーID</returns>
+        public static UserId ValueOf(string value)
+        {
+            Validate(value);
+            return new UserId(value);
+        }
+
+        /// <summary>
+        /// ULID形式の新しいユーザーIDを生成します。
+        /// </summary>
+        /// <returns>
+        /// 新しく生成したユーザーIDを返します。
+        /// </returns>
+        public static UserId NewId()
+        {
+            return new UserId(ULID.NewULID().Value);
+        }
+
+        private static void Validate(string value)
+        {
+            if (value.Length != Length) throw new Exception($"ユーザーIDは{Length}文字である必要があります。");
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Users/UserId_Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Users;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Users
+{
+    /// <summary>
+    /// ユーザーID - test
+    /// </summary>
+    public class UserId_Test
+    {
+        [Fact(DisplayName = "[ValueOf()] 指定した値でユーザーIDが生成される")]
+        public void ValueOf_Test1()
+        {
+            // arrange
+            var value = "12345678901234567890123456";
+
+            // act
+            var id = UserId.ValueOf(value);
+
+            // assert
+            Assert.Equal(value, id.Value);
+        }
+
+        [Theory(DisplayName = "[ValueOf()] 26文字以外の値を渡すと例外が発生する")]
+        [InlineData("1234567890123456789012345")]
+        [InlineData("123456789012345678901234567")]
+        public void ValueOf_Test2(string value)
+        {
+            // act
+            var target = () => UserId.ValueOf(value);
+
+            // assert
+            var ex = Assert.Throws<Exception>(target);
+            Assert.Equal("ユーザーIDは26文字である必要があります。", ex.Message);
+        }
+
+        [Fact(DisplayName = "[NewId()] 新しいIDがULID形式で生成される")]
+        public void NewId_Test1()
+        {
+            // act
+            var id = UserId.NewId();
+
+            // assert
+            Assert.Equal(26, id.Value.Length);
+        }
+    }
+}
