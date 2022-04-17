9d7424f5c7a3873aae175ccade7fa386ab34ffbb
uramibushi <uramibushi701@gmail.com>
2022-04-17 17:27:55 +0900

    日付期間（値オブジェクト）作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/PlanPeriod.cs
@@ -0,0 +1,61 @@
+﻿namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// 予定期間
+    /// </summary>
+    public record PlanPeriod
+    {
+        private PlanPeriod(DateTime startDate, DateTime endDate)
+        {
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
+
+        /// <summary>
+        /// 予定開始日
+        /// </summary>
+        public DateTime StartDate { get; }
+
+        /// <summary>
+        /// 予定終了日
+        /// </summary>
+        public DateTime EndDate { get; }
+
+        /// <summary>
+        /// 指定した値で予定期間を生成します。
+        /// </summary>
+        /// <param name="startDate">予定開始日</param>
+        /// <param name="endDate">予定終了日</param>
+        /// <returns>予定期間</returns>
+        public static PlanPeriod ValueOf(DateTime startDate, DateTime endDate)
+        {
+            var roundedStartDate = RoundDate(startDate);
+            var roundedEndDate = RoundDate(endDate);
+            Validate(roundedStartDate, roundedEndDate);
+            return new PlanPeriod(roundedStartDate, roundedEndDate);
+        }
+
+        /// <summary>
+        /// 日付を丸めます。
+        /// 時間は切り捨てして日付のみにします。
+        /// </summary>
+        /// <param name="value">日付</param>
+        /// <returns>丸めた結果の日付</returns>
+        private static DateTime RoundDate(DateTime value)
+        {
+            return value.Date;
+        }
+
+        /// <summary>
+        /// 値を検証します。
+        /// 予定開始日と終了日の
+        /// </summary>
+        /// <param name="startDate">予定開始日b</param>
+        /// <param name="endDate">予定終了日</param>
+        /// <exception cref="Exception">日付に矛盾がある場合、例外を投げます</exception>
+        private static void Validate(DateTime startDate, DateTime endDate)
+        {
+            if (startDate > endDate) throw new Exception("開始日と終了日の前後関係に誤りがあります。");
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/PlanPeriod_Test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// PlanPeriod - test
+    /// </summary>
+    public class PlanPeriod_Test
+    {
+        [Fact(DisplayName = "[ValueOf(startDate, endDate)] 指定した値で予定期間を生成できる")]
+        public void ValueOf_Test1()
+        {
+            // arrange
+            var startDate = new DateTime(2020, 1, 1);
+            var endDate = new DateTime(2022, 3, 31);
+
+            // act
+            var planPeriod = PlanPeriod.ValueOf(startDate, endDate);
+
+            // assert
+            Assert.Equal(startDate, planPeriod.StartDate);
+            Assert.Equal(endDate, planPeriod.EndDate);
+        }
+
+        [Fact(DisplayName = "[ValueOf(startDate, endDate)] 指定日時の時間部分は丸められて日付のみになる")]
+        public void ValueOf_Test2()
+        {
+            // arrange
+            var startDate = new DateTime(2020, 1, 1, 11, 12, 13);
+            var endDate = new DateTime(2022, 3, 31, 14, 15, 16);
+
+            // act
+            var planPeriod = PlanPeriod.ValueOf(startDate, endDate);
+
+            // asserts
+            Assert.NotEqual(startDate, planPeriod.StartDate);
+            Assert.NotEqual(endDate, planPeriod.EndDate);
+            Assert.Equal(startDate.Date, planPeriod.StartDate);
+            Assert.Equal(endDate.Date, planPeriod.EndDate);
+        }
+
+        [Fact(DisplayName = "[ValueOf(startDate, endDate)] 開始日が終了日の未来日の場合、例外が投げられる")]
+        public void ValueOf_Test3()
+        {
+            // arrange
+            var startDate = new DateTime(2022, 4, 1);
+            var endDate = new DateTime(2022, 3, 31);
+
+            // act
+            var target = () => PlanPeriod.ValueOf(startDate, endDate);
+
+            // asserts
+            var ex = Assert.Throws<Exception>(target);
+            Assert.Equal("開始日と終了日の前後関係に誤りがあります。", ex.Message);
+        }
+
+        [Fact(DisplayName = "[ValueOf(startDate, endDate)] 開始日が終了日が同日の場合は日付期間を生成できる")]
+        public void ValueOf_Test4()
+        {
+            // arrange
+            var startDate = new DateTime(2022, 4, 1);
+            var endDate = new DateTime(2022, 4, 1);
+
+            // act
+            var planPeriod = PlanPeriod.ValueOf(startDate, endDate);
+
+            // asserts
+            Assert.Equal(startDate, planPeriod.StartDate);
+            Assert.Equal(endDate, planPeriod.EndDate);
+        }
+    }
+}
