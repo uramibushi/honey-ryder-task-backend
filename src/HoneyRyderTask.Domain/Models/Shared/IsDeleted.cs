15d2a2b6ade54949b244f6c728812a563fa3872e
uramibushi <uramibushi701@gmail.com>
2022-04-17 16:53:46 +0900

    IsDeleted（値オブジェクト）作成

---
+++ src/HoneyRyderTask.Domain/Models/Shared/IsDeleted.cs
@@ -0,0 +1,58 @@
+﻿namespace HoneyRyderTask.Domain.Models.Shared
+{
+    /// <summary>
+    /// 削除されているかを表します。
+    /// 削除されていれば true。それ以外は false。
+    /// </summary>
+    public record IsDeleted
+    {
+        private IsDeleted(bool value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// 削除されているかを表します。
+        /// 削除されていれば true。それ以外は false。
+        /// </summary>
+        public bool Value { get; }
+
+        /// <summary>
+        /// 削除されているかの値を生成します。
+        /// </summary>
+        /// <param name="value">指定値</param>
+        /// <returns>生成した値を返します。</returns>
+        public static IsDeleted ValueOf(bool value)
+        {
+            return new IsDeleted(value);
+        }
+
+        /// <summary>
+        /// IsDeletedの値とboole値が一致しているかの真偽値を返します。
+        /// 一致している場合は true。一致していない場合は false を返します。
+        /// </summary>
+        /// <param name="isDeleted">IsDeleted</param>
+        /// <param name="boolValue">Bool値</param>
+        /// <returns>
+        /// IsDeletedの値とbool値が一致している場合は true。一致していない場合は false を返します。
+        /// </returns>
+        public static bool operator ==(IsDeleted isDeleted, bool boolValue)
+        {
+            return isDeleted.Value == boolValue;
+        }
+
+        /// <summary>
+        /// IsDeletedの値とboole値が一致していないかの真偽値を返します。
+        /// 一致していない場合は true。一致している場合は false を返します。
+        /// </summary>
+        /// <param name="isDeleted">IsDeleted</param>
+        /// <param name="boolValue">Bool値</param>
+        /// <returns>
+        /// IsDeletedの値とbool値が一致していない場合は true。一致している場合は false を返します。
+        /// </returns>
+        public static bool operator !=(IsDeleted isDeleted, bool boolValue)
+        {
+            return isDeleted.Value != boolValue;
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Shared/IsDeleted_Test.cs
@@ -0,0 +1,55 @@
+﻿using HoneyRyderTask.Domain.Models.Shared;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Shared
+{
+    /// <summary>
+    /// IsDeleted - test
+    /// </summary>
+    public class IsDeleted_Test
+    {
+        [Fact(DisplayName = "[ValueOf()] 指定した値でIsDeletedを生成できる")]
+        public void ValueOf_Test1()
+        {
+            // act
+            var isDeleted = IsDeleted.ValueOf(true);
+
+            // assert
+            Assert.True(isDeleted.Value);
+        }
+
+        [Theory(DisplayName = "[==(isDeleted, boolValue)] IsDeletedとBool値が等しいか判断できる")]
+        [InlineData(true, false, false)]
+        [InlineData(true, true, true)]
+        [InlineData(false, false, true)]
+        [InlineData(false, true, false)]
+        public void Equals_Test1(bool value, bool boolValue, bool expected)
+        {
+            // arrange
+            var isDeleted = IsDeleted.ValueOf(value);
+
+            // act
+            var actual = isDeleted == boolValue;
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory(DisplayName = "[!=(isDeleted, boolValue)] IsDeletedとBool値が等しくないか判断できる")]
+        [InlineData(true, false, true)]
+        [InlineData(true, true, false)]
+        [InlineData(false, false, false)]
+        [InlineData(false, true, true)]
+        public void NotEquals_Test1(bool value, bool boolValue, bool expected)
+        {
+            // arrange
+            var isDeleted = IsDeleted.ValueOf(value);
+
+            // act
+            var actual = isDeleted != boolValue;
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
