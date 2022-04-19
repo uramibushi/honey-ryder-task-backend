f3e22744fdba5dca99465f5caa00bd690aedfe7a
uramibushi <uramibushi701@gmail.com>
2022-04-19 23:46:36 +0900

    タスク期限（値オブジェクト）作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskDueDate.cs
@@ -0,0 +1,42 @@
+﻿namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク期限
+    /// </summary>
+    public record TaskDueDate
+    {
+        private TaskDueDate(DateTime value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// 指定した値でタスク期限を生成します。
+        /// </summary>
+        /// <param name="value">指定値</param>
+        /// <returns>
+        /// タスク期限
+        /// </returns>
+        public static TaskDueDate ValueOf(DateTime value)
+        {
+            var roundedDate = RoundDate(value);
+            return new TaskDueDate(roundedDate);
+        }
+
+        /// <summary>
+        /// タスク期限s
+        /// </summary>
+        public DateTime Value { get; }
+
+        /// <summary>
+        /// 日付を丸めます。
+        /// 時間は切り捨てして日付のみにします。
+        /// </summary>
+        /// <param name="value">日付</param>
+        /// <returns>丸めた結果の日付</returns>
+        private static DateTime RoundDate(DateTime value)
+        {
+            return value.Date;
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/TaskDueDate_Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク期限 - test
+    /// </summary>
+    public class TaskDueDate_Test
+    {
+        [Fact(DisplayName = "[ValueOf(value)] 指定した値でタスク期限を生成できる")]
+        public void ValueOf_Test1()
+        {
+            // arrange
+            var value = new DateTime(2020, 1, 1);
+
+            // act
+            var taskDueDate = TaskDueDate.ValueOf(value);
+
+            // assert
+            Assert.Equal(value, taskDueDate.Value);
+        }
+
+        [Fact(DisplayName = "[ValueOf(value)] 指定日時の時間部分は丸められて日付のみになる")]
+        public void ValueOf_Test2()
+        {
+            // arrange
+            var value = new DateTime(2020, 1, 1, 11, 12, 13);
+
+            // act
+            var taskDueDate = TaskDueDate.ValueOf(value);
+
+            // asserts
+            Assert.NotEqual(value, taskDueDate.Value);
+            Assert.Equal(value.Date, taskDueDate.Value);
+        }
+    }
+}
