c01cf9c31532085512e1e7efe646007305350062
uramibushi <uramibushi701@gmail.com>
2022-04-24 14:48:54 +0900

    タスク予定時間（値オブジェクト）名前変更
    WorkingTime → TaskScheduleTime

--- src/HoneyRyderTask.Domain/Models/Tasks/TaskScheduleTime.cs
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskScheduleTime.cs
@@ -3,36 +3,36 @@
 namespace HoneyRyderTask.Domain.Models.Tasks
 {
     /// <summary>
-    /// 作業時間
+    /// タスク予定時間
     /// </summary>
-    public record WorkingTime
+    public record TaskScheduleTime
     {
-        private WorkingTime(decimal value)
+        private TaskScheduleTime(decimal value)
         {
             this.Value = value;
         }
 
         /// <summary>
-        /// 作業時間（h)
+        /// タスク予定時間（h)
         /// </summary>
         public decimal Value { get; }
 
         /// <summary>
-        /// 指定した値で作業時間を生成します。
+        /// 指定した値でタスク予定時間を生成します。
         /// </summary>
         /// <param name="value">指定値</param>
         /// <returns>
-        /// 作業時間
+        /// タスク予定時間
         /// </returns>
-        public static WorkingTime ValueOf(decimal value)
+        public static TaskScheduleTime ValueOf(decimal value)
         {
             var adjustedValue = AdjustValue(value);
-            return new WorkingTime(adjustedValue);
+            return new TaskScheduleTime(adjustedValue);
         }
 
         /// <summary>
         /// 値を調整する。
-        /// 作業時間の小数部は２桁までとする。３桁以上は切り捨てを行う。
+        /// タスク予定時間の小数部は２桁までとする。３桁以上は切り捨てを行う。
         /// </summary>
         /// <param name="value">調整を行う値。</param>
         /// <returns>調整後の値</returns>
--- src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/TaskScheduleTime_Test.cs
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/TaskScheduleTime_Test.cs
@@ -5,31 +5,31 @@ using Xunit;
 namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
 {
     /// <summary>
-    /// 作業時間 - test
+    /// タスク予定時間 - test
     /// </summary>
-    public class WorkingTime_Test
+    public class TaskScheduleTime_Test
     {
-        [Fact(DisplayName = "[ValueOf()] 指定した値で作業時間を生成できる")]
+        [Fact(DisplayName = "[ValueOf()] 指定した値でタスク予定時間を生成できる")]
         public void ValueOf_Test1()
         {
             // arrange
             var value = 1.25M;
 
             // act
-            var workingTime = WorkingTime.ValueOf(value);
+            var workingTime = TaskScheduleTime.ValueOf(value);
 
             // assert
             Assert.Equal(value, workingTime.Value);
         }
 
-        [Fact(DisplayName = "[ValueOf()] 指定した値の小数部３桁以上の値は切り捨てられる")]
+        [Fact(DisplayName = "[ValueOf()] 指定した値の小数部２桁まで。それ以上の値は切り捨てられる")]
         public void ValueOf_Test2()
         {
             // arrange
             var value = 1.255M;
 
             // act
-            var workingTime = WorkingTime.ValueOf(value);
+            var workingTime = TaskScheduleTime.ValueOf(value);
 
             // assert
             Assert.Equal(1.25M, workingTime.Value);
