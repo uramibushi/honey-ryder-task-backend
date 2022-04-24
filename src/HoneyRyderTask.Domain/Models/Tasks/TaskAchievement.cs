a13e4b367a12b91d0437e48903a5dca291899931
uramibushi <uramibushi701@gmail.com>
2022-04-24 18:02:45 +0900

    タスク実績（値オブジェクト）作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskAchievement.cs
@@ -0,0 +1,38 @@
+﻿namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク実績
+    /// </summary>
+    public record TaskAchievement
+    {
+        private TaskAchievement(DateTime startDate, DateTime endDate, decimal? time)
+        {
+            this.Period = TaskAchievementPeriod.ValueOf(startDate, endDate);
+            this.Time = time.HasValue ? TaskAchievementTime.ValueOf(time.Value) : null;
+        }
+
+        /// <summary>
+        /// タスク実績期間
+        /// </summary>
+        public TaskAchievementPeriod Period { get; }
+
+        /// <summary>
+        /// タスク実績時間
+        /// </summary>
+        public TaskAchievementTime? Time { get; }
+
+        /// <summary>
+        /// 指定の値でタスク実績を生成します。
+        /// </summary>
+        /// <param name="startDate">開始日</param>
+        /// <param name="endDate">終了日</param>
+        /// <param name="time">時間</param>
+        /// <returns>
+        /// タスク実績
+        /// </returns>
+        public static TaskAchievement ValueOf(DateTime startDate, DateTime endDate, decimal? time)
+        {
+            return new TaskAchievement(startDate, endDate, time);
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/TaskAchievement_Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク実績 - test
+    /// </summary>
+    public class TaskAchievement_Test
+    {
+        [Fact(DisplayName = "[ValueOf(startDate, endDate, time) 指定した値でタスク実績を生成できる")]
+        public void ValueOf_Test1()
+        {
+            // arrange
+            var startDate = new DateTime(2022, 4, 1);
+            var endDate = new DateTime(2022, 4, 3);
+            var time = 24M;
+
+            // act
+            var taskAchievement = TaskAchievement.ValueOf(startDate, endDate, time);
+
+            // assert
+            Assert.Equal(startDate, taskAchievement.Period.StartDate);
+            Assert.Equal(endDate, taskAchievement.Period.EndDate);
+            Assert.Equal(time, taskAchievement.Time?.Value);
+        }
+
+        [Fact(DisplayName = "[ValueOf(startDate, endDate, time) タスク実績時間は省略可能")]
+        public void ValueOf_Test2()
+        {
+            // arrange
+            var startDate = new DateTime(2022, 4, 1);
+            var endDate = new DateTime(2022, 4, 3);
+            decimal? time = null;
+
+            // act
+            var taskAchievement = TaskAchievement.ValueOf(startDate, endDate, time);
+
+            // assert
+            Assert.Equal(startDate, taskAchievement.Period.StartDate);
+            Assert.Equal(endDate, taskAchievement.Period.EndDate);
+            Assert.Null(taskAchievement.Time);
+        }
+    }
+}
