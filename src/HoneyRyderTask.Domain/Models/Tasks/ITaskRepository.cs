67fae13ce65b2eb0e6468b0a54ef3a0df61f75d7
uramibushi <uramibushi701@gmail.com>
2022-04-24 21:28:34 +0900

    タスクリポジトリ（インタフェース）作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク リポジトリ
+    /// </summary>
+    public interface ITaskRepository
+    {
+        /// <summary>
+        /// タスクをリポジトリに追加します。
+        /// </summary>
+        /// <param name="task">タスク</param>
+        void Add(Task task);
+    }
+}
