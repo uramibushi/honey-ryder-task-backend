0618f35b34347f3a104712a531372298b387fccc
uramibushi <uramibushi701@gmail.com>
2022-04-24 17:28:40 +0900

    タスク実績時間（値オブジェクト）作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskAchievementTime.cs
@@ -0,0 +1,44 @@
+﻿using HoneyRyderTask.Domain.Helpers.Extensions;
+
+namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク実績時間
+    /// </summary>
+    public class TaskAchievementTime
+    {
+        private TaskAchievementTime(decimal value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// タスク実績時間（h)
+        /// </summary>
+        public decimal Value { get; }
+
+        /// <summary>
+        /// 指定した値でタスク実績時間を生成します。
+        /// </summary>
+        /// <param name="value">指定値</param>
+        /// <returns>
+        /// タスク実績時間
+        /// </returns>
+        public static TaskAchievementTime ValueOf(decimal value)
+        {
+            var adjustedValue = AdjustValue(value);
+            return new TaskAchievementTime(adjustedValue);
+        }
+
+        /// <summary>
+        /// 値を調整する。
+        /// タスク実績時間の小数部は２桁までとする。３桁以上は切り捨てを行う。
+        /// </summary>
+        /// <param name="value">調整を行う値。</param>
+        /// <returns>調整後の値</returns>
+        private static decimal AdjustValue(decimal value)
+        {
+            return value.Truncate(2);
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/TaskAchievementTime_Test.cs
@@ -0,0 +1,37 @@
+﻿using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク実績時間 - test
+    /// </summary>
+    public class TaskAchievementTime_Test
+    {
+        [Fact(DisplayName = "[ValueOf()] 指定した値でタスク実績時間を生成できる")]
+        public void ValueOf_Test1()
+        {
+            // arrange
+            var value = 1.25M;
+
+            // act
+            var workingTime = TaskAchievementTime.ValueOf(value);
+
+            // assert
+            Assert.Equal(value, workingTime.Value);
+        }
+
+        [Fact(DisplayName = "[ValueOf()] 指定した値の小数部２桁まで。それ以上の値は切り捨てられる")]
+        public void ValueOf_Test2()
+        {
+            // arrange
+            var value = 1.255M;
+
+            // act
+            var workingTime = TaskAchievementTime.ValueOf(value);
+
+            // assert
+            Assert.Equal(1.25M, workingTime.Value);
+        }
+    }
+}
