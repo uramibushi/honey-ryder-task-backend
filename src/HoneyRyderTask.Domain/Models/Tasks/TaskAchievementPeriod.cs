62f879cbe9702cc9ab96c009ffb2f59efaa3e7c7
uramibushi <uramibushi701@gmail.com>
2022-04-24 17:16:31 +0900

    タスク実績期間（値オブジェクト）作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/TaskAchievementPeriod.cs
@@ -0,0 +1,62 @@
+﻿namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク実績期間
+    /// </summary>
+    public class TaskAchievementPeriod
+    {
+        private TaskAchievementPeriod(DateTime startDate, DateTime? endDate)
+        {
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
+
+        /// <summary>
+        /// タスク実績開始日
+        /// </summary>
+        public DateTime StartDate { get; }
+
+        /// <summary>
+        /// タスク実績終了日
+        /// </summary>
+        public DateTime? EndDate { get; }
+
+        /// <summary>
+        /// 指定した値でタスク実績期間を生成します。
+        /// </summary>
+        /// <param name="startDate">実績開始日</param>
+        /// <param name="endDate">実績終了日</param>
+        /// <returns>タスク実績期間</returns>
+        public static TaskAchievementPeriod ValueOf(DateTime startDate, DateTime? endDate)
+        {
+            var roundedStartDate = RoundDate(startDate);
+            DateTime? roundedEndDate = endDate.HasValue ? RoundDate(endDate.Value) : null;
+            Validate(roundedStartDate, roundedEndDate);
+            return new TaskAchievementPeriod(roundedStartDate, roundedEndDate);
+        }
+
+        /// <summary>
+        /// 日付を丸めます。
+        /// 時間は切り捨てして日付のみにします。
+        /// </summary>
+        /// <param name="value">日付</param>
+        /// <returns>丸めた結果の日付</returns>
+        private static DateTime RoundDate(DateTime value)
+        {
+            return value.Date;
+        }
+
+        /// <summary>
+        /// 値を検証します。
+        /// タスク実績開始日と終了日の
+        /// </summary>
+        /// <param name="startDate">実績開始日</param>
+        /// <param name="endDate">実績終了日</param>
+        /// <exception cref="Exception">日付に矛盾がある場合、例外を投げます</exception>
+        private static void Validate(DateTime startDate, DateTime? endDate)
+        {
+            if (!endDate.HasValue) return;
+            if (startDate > endDate) throw new Exception("開始日と終了日の前後関係に誤りがあります。");
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/TaskAchievementPeriod_Test.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク実績期間 - test
+    /// </summary>
+    public class TaskAchievementPeriod_Test
+    {
+        [Fact(DisplayName = "[ValueOf(startDate, endDate) 指定した値でタスク実績を生成できる")]
+        public void ValueOf_Test1()
+        {
+            // arrange
+            var startDate = new DateTime(2022, 4, 1);
+            var endDate = new DateTime(2022, 4, 3);
+
+            // act
+            var actual = TaskAchievementPeriod.ValueOf(startDate, endDate);
+
+            // assert
+            Assert.Equal(startDate, actual.StartDate);
+            Assert.Equal(endDate, actual.EndDate);
+        }
+
+        [Fact(DisplayName = "[ValueOf(startDate, endDate) 終了日は省略可能")]
+        public void ValueOf_Test2()
+        {
+            // arrange
+            var startDate = new DateTime(2022, 4, 1);
+            DateTime? endDate = null;
+
+            // act
+            var actual = TaskAchievementPeriod.ValueOf(startDate, endDate);
+
+            // assert
+            Assert.Equal(startDate, actual.StartDate);
+            Assert.Null(actual.EndDate);
+        }
+
+        [Fact(DisplayName = "[ValueOf(startDate, endDate)] 指定日時の時間部分は丸められて日付のみになる")]
+        public void ValueOf_Test3()
+        {
+            // arrange
+            var startDate = new DateTime(2020, 1, 1, 11, 12, 13);
+            var endDate = new DateTime(2022, 3, 31, 14, 15, 16);
+
+            // act
+            var actual = TaskAchievementPeriod.ValueOf(startDate, endDate);
+
+            // asserts
+            Assert.NotEqual(startDate, actual.StartDate);
+            Assert.NotEqual(endDate, actual.EndDate);
+            Assert.Equal(startDate.Date, actual.StartDate);
+            Assert.Equal(endDate.Date, actual.EndDate);
+        }
+
+        [Fact(DisplayName = "[ValueOf(startDate, endDate)] 開始日が終了日の未来日の場合、例外が投げられる")]
+        public void ValueOf_Test4()
+        {
+            // arrange
+            var startDate = new DateTime(2022, 4, 1);
+            var endDate = new DateTime(2022, 3, 31);
+
+            // act
+            var target = () => TaskAchievementPeriod.ValueOf(startDate, endDate);
+
+            // asserts
+            var ex = Assert.Throws<Exception>(target);
+            Assert.Equal("開始日と終了日の前後関係に誤りがあります。", ex.Message);
+        }
+
+        [Fact(DisplayName = "[ValueOf(startDate, endDate)] 開始日が終了日が同日の場合は日付期間を生成できる")]
+        public void ValueOf_Test5()
+        {
+            // arrange
+            var startDate = new DateTime(2022, 4, 1);
+            var endDate = new DateTime(2022, 4, 1);
+
+            // act
+            var actual = TaskAchievementPeriod.ValueOf(startDate, endDate);
+
+            // asserts
+            Assert.Equal(startDate, actual.StartDate);
+            Assert.Equal(endDate, actual.EndDate);
+        }
+    }
+}
