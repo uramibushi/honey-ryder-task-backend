8d5a2e798aa2abd6f07b0c2ddf1f25b97405356e
uramibushi <uramibushi701@gmail.com>
2022-04-24 21:23:05 +0900

    タスク（エンティティ）作成

---
+++ src/HoneyRyderTask.Domain/Models/Tasks/Task.cs
@@ -0,0 +1,130 @@
+﻿using HoneyRyderTask.Domain.Models.Shared;
+
+namespace HoneyRyderTask.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク - entity
+    /// </summary>
+    public class Task
+    {
+        private Task(
+            TaskId id,
+            TaskTitle title,
+            TaskStatus status,
+            TaskDetail? detail,
+            TaskDueDate? dueDate,
+            TaskSchedule? schedule,
+            TaskAchievement? achievement,
+            IsDeleted isDeleted)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Status = status;
+            this.Detail = detail;
+            this.DueDate = dueDate;
+            this.Schedule = schedule;
+            this.Achievement = achievement;
+            this.IsDeleted = isDeleted;
+        }
+
+        /// <summary>
+        /// タスクID
+        /// </summary>
+        public TaskId Id { get; }
+
+        /// <summary>
+        /// タスクタイトル
+        /// </summary>
+        public TaskTitle Title { get; }
+
+        /// <summary>
+        /// タスク状態
+        /// </summary>
+        public TaskStatus Status { get; }
+
+        /// <summary>
+        /// タスク詳細
+        /// </summary>
+        public TaskDetail? Detail { get; }
+
+        /// <summary>
+        /// タスク期限
+        /// </summary>
+        public TaskDueDate? DueDate { get; }
+
+        /// <summary>
+        /// タスク予定
+        /// </summary>
+        public TaskSchedule? Schedule { get; }
+
+        /// <summary>
+        /// タスク実績
+        /// </summary>
+        public TaskAchievement? Achievement { get; }
+
+        /// <summary>
+        /// 削除済みであるかを示す値
+        /// </summary>
+        public IsDeleted IsDeleted { get; }
+
+        /// <summary>
+        /// 新しいタスクを作成します。
+        /// </summary>
+        /// <param name="title">タスクタイトル</param>
+        /// <param name="detail">タスク詳細</param>
+        /// <param name="dueDate">タスク期限</param>
+        /// <returns>
+        /// 新しく作成したタスクを返します。
+        /// </returns>
+        public static Task CreateNewTask(
+            TaskTitle title,
+            TaskDetail detail,
+            TaskDueDate dueDate)
+        {
+            return new Task(
+                id: TaskId.NewId(),
+                title: title,
+                status: TaskStatus.ToDo,
+                detail: detail,
+                dueDate: dueDate,
+                schedule: null,
+                achievement: null,
+                isDeleted: IsDeleted.ValueOf(false));
+        }
+
+        /// <summary>
+        /// タスクを再構築します。
+        /// </summary>
+        /// <param name="id">タスクID</param>
+        /// <param name="title">タスクタイトル</param>
+        /// <param name="status">タスク状態</param>
+        /// <param name="detail">タスク詳細</param>
+        /// <param name="dueDate">タスク期限</param>
+        /// <param name="schedule">タスク予定</param>
+        /// <param name="achievement">タスク実績</param>
+        /// <param name="isDeleted">削除済み</param>
+        /// <returns>
+        /// 再構築したタスクを返します。
+        /// </returns>
+        public static Task Reconstruct(
+            TaskId id,
+            TaskTitle title,
+            TaskStatus status,
+            TaskDetail? detail,
+            TaskDueDate? dueDate,
+            TaskSchedule? schedule,
+            TaskAchievement? achievement,
+            IsDeleted isDeleted)
+        {
+            return new Task(
+                id: id,
+                title: title,
+                status: status,
+                detail: detail,
+                dueDate: dueDate,
+                schedule: schedule,
+                achievement: achievement,
+                isDeleted: isDeleted);
+        }
+    }
+}
---
+++ src/HoneyRyderTaskTests/Tests/Domain/Models/Tasks/Task_Test.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using HoneyRyderTask.Domain.Models.Shared;
+using HoneyRyderTask.Domain.Models.Tasks;
+using Xunit;
+
+namespace HoneyRyderTaskTests.Tests.Domain.Models.Tasks
+{
+    /// <summary>
+    /// タスク - test
+    /// </summary>
+    public class Task_Test
+    {
+        [Fact(DisplayName = "[CreateNewTask()] 指定した値でタスクが作成される")]
+        public void CreateNewTask_Test1()
+        {
+            // arrange
+            var title = TaskTitle.ValueOf("タスクタイトル");
+            var detail = TaskDetail.ValueOf("タスク詳細");
+            var dueDate = TaskDueDate.ValueOf(new DateTime(2022, 4, 1));
+
+            // act
+            var task = Task.CreateNewTask(title, detail, dueDate);
+
+            // assert
+            Assert.Equal(title, task.Title);
+            Assert.Equal(detail, task.Detail);
+            Assert.Equal(dueDate, task.DueDate);
+        }
+
+        [Fact(DisplayName = "[CreateNewTask()] 「タスクID」は新規採番される")]
+        public void CreateNewTask_Test2()
+        {
+            // arrange
+            var title = TaskTitle.ValueOf("タスクタイトル");
+            var detail = TaskDetail.ValueOf("タスク詳細");
+            var dueDate = TaskDueDate.ValueOf(new DateTime(2022, 4, 1));
+
+            // act
+            var task = Task.CreateNewTask(title, detail, dueDate);
+
+            // assert
+            Assert.Equal(26, task.Id.Value.Length);
+        }
+
+        [Fact(DisplayName = "[CreateNewTask()] 「タスク状態」は 未着手 で作成される")]
+        public void CreateNewTask_Test3()
+        {
+            // arrange
+            var title = TaskTitle.ValueOf("タスクタイトル");
+            var detail = TaskDetail.ValueOf("タスク詳細");
+            var dueDate = TaskDueDate.ValueOf(new DateTime(2022, 4, 1));
+
+            // act
+            var task = Task.CreateNewTask(title, detail, dueDate);
+
+            // assert
+            Assert.Equal(TaskStatus.ToDo, task.Status);
+        }
+
+        [Fact(DisplayName = "[CreateNewTask()] 「削除済み」は false で作成される")]
+        public void CreateNewTask_Test4()
+        {
+            // arrange
+            var title = TaskTitle.ValueOf("タスクタイトル");
+            var detail = TaskDetail.ValueOf("タスク詳細");
+            var dueDate = TaskDueDate.ValueOf(new DateTime(2022, 4, 1));
+
+            // act
+            var task = Task.CreateNewTask(title, detail, dueDate);
+
+            // assert
+            Assert.False(task.IsDeleted.Value);
+        }
+
+        [Fact(DisplayName = "[CreateNewTask()] 「タスク予定」「タスク実績」は未設定で作成される")]
+        public void CreateNewTask_Test5()
+        {
+            // arrange
+            var title = TaskTitle.ValueOf("タスクタイトル");
+            var detail = TaskDetail.ValueOf("タスク詳細");
+            var dueDate = TaskDueDate.ValueOf(new DateTime(2022, 4, 1));
+
+            // act
+            var task = Task.CreateNewTask(title, detail, dueDate);
+
+            // assert
+            Assert.Null(task.Schedule);
+            Assert.Null(task.Achievement);
+        }
+
+        [Fact(DisplayName = "[Reconstruct()] 指定した値でタスクを再構築できる")]
+        public void Reconstruct_Test1()
+        {
+            // arrange
+            var id = TaskId.NewId();
+            var status = TaskStatus.Done;
+            var title = TaskTitle.ValueOf("タスクタイトル");
+            var detail = TaskDetail.ValueOf("タスク詳細");
+            var dueDate = TaskDueDate.ValueOf(new DateTime(2022, 4, 1));
+            var schedule = TaskSchedule.ValueOf(
+                new DateTime(2022, 4, 1),
+                new DateTime(2022, 4, 2),
+                16M);
+            var achievement = TaskAchievement.ValueOf(
+                new DateTime(2022, 4, 2),
+                new DateTime(2022, 4, 4),
+                24M);
+            var isDeleted = IsDeleted.ValueOf(false);
+
+            // act
+            var task = Task.Reconstruct(
+                id,
+                title,
+                status,
+                detail,
+                dueDate,
+                schedule,
+                achievement,
+                isDeleted);
+
+            // assert
+            Assert.Equal(id, task.Id);
+            Assert.Equal(title, task.Title);
+            Assert.Equal(status, task.Status);
+            Assert.Equal(detail, task.Detail);
+            Assert.Equal(dueDate, task.DueDate);
+            Assert.Equal(schedule, task.Schedule);
+            Assert.Equal(achievement, task.Achievement);
+            Assert.Equal(isDeleted, task.IsDeleted);
+        }
+    }
+}
